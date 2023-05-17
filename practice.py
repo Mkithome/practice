fcb96a9c6ab1cbf35ccbc3179e308b7182906595
Martin Kithome <mkithome1@gmail.com>
2023-05-17 20:46:49 +0300

    create and write python code

---
+++ practice.py
@@ -0,0 +1,9 @@
+movies = ["300" , "bourne legacy" , "extraction" , "fast and furious"]
+print(movies)
+
+name_jina = 'martin bilal'
+print(type,(name_jina))
+print("Hello World")
+my_students=["Bilal" , "Bena"]
+print(my_students)
+      
